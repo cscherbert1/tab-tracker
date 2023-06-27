919b3e3a5c3de10c09fe6189f96a7adcdfc6f7f1
Collin Scherbert <cscherbert1@gmail.com>
2023-06-26 23:13:17 -0500

    Validating registration

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -1,7 +1,15 @@
+const {User} = require('../models')
+
 module.exports = {
-    register (req, res) {
-        res.send({
-            message: `Email registered: ${req.body.email}. Have fun!`
-        })
+    async register (req, res) {
+        try {
+            const user = await User.create(req.body)
+            res.send(user.toJSON())
+        } catch (err) {
+            //email exists
+            res.status(400).send({
+                error: 'This email is already in use.'
+            })
+        }        
     }
 }
