7417153da9eb93aaf6abf1e27ebc6ebc1c9351b8
Collin Scherbert <cscherbert1@gmail.com>
2023-06-26 22:40:15 -0500

    Added AuthenticationController

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,13 +1,12 @@
+const AuthenticationController = require('./controllers/AuthenticationController')
 module.exports = (app) => {
-    app.get('/smoke', (req, res) => {
-        res.send({
-            message: 'tab-tracker server is running'
-        })
-    })
+    app.post('/register', 
+        AuthenticationController.register
+    )
+    // app.get('/smoke', (req, res) => {
+    //     res.send({
+    //         message: 'tab-tracker server is running'
+    //     })
+    // })
     
-    app.post('/register', (req, res) => {
-        res.send({
-            message: `Email registered: ${req.body.email}. Have fun!`
-        })
-    })
 }
